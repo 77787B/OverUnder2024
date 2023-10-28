1305ab24e53c5bae18268054bbb14f2dcdf6a061
fezhou2 <fzhou99@gmail.com>
2023-10-28 03:55:33 -0700

    Update auto-far-4balls.cpp

--- UnderOver/src/autonomous/auto-far-4balls.cpp
+++ UnderOver/src/autonomous/auto-far-4balls.cpp
@@ -6,37 +6,37 @@
 
 void auton_far_4balls() {
   //pickup first ball
-  setIntakeSpeed(90);
-  MyGps.gpsPIDMove(595, -1160, -1, 70);
+  setIntakeSpeed(80);
+  MyGps.gpsPIDMove(600, -1170, -1, 70);
   PIDAngleRotateAbs(-35.0);
   timerForward(-40, 200);
   PIDAngleRotateAbs(-90.0);
-  timerForward(-40, 400);
-  setIntakeSpeed(20);
+  timerForward(-40, 250);
+  setIntakeSpeed(30);
 
   ///push first ball
   MyGps.resetForwardPosGps();
   MyGps.gpsPIDMove(-820, 0, 1, 70, "heading");
 
   //second ball
-  timerForward(-50, 200);
-  PIDAngleRotateAbs(90.0);
+  timerForward(-60, 100);
+  PIDAngleRotateAbs(90.0, 1.5);
   setIntakeSpeed(-100);
-  this_thread::sleep_for(600);
-  timerForward(-100, 400);
+  this_thread::sleep_for(500);
+  timerForward(-100, 250);
 
   //third ball
-   setIntakeSpeed(100);
-   MyGps.gpsPIDMove(-500, 800, -1, 60);
-  PIDAngleRotateAbs(-90.0, 2, 0.4, 35, 1.5);
-  timerForward(-40, 400);
-   setIntakeSpeed(20);
+   setIntakeSpeed(90);
+   MyGps.gpsPIDMove(-550, 770, -1, 60);
+  PIDAngleRotateAbs(-100.0, 2, 0.4, 35, 1.5);
+  timerForward(-60, 450);
+   setIntakeSpeed(30);
    MyGps.gpsPIDMove(-500, 520, -1, 70, "heading");
 
   PIDAngleRotateAbs(90.0, 2, 0.4, 35, 1.5);
   setIntakeSpeed(-100);
-  this_thread::sleep_for(500);
-  timerForward(-100, 800);
+  this_thread::sleep_for(300);
+  timerForward(-100, 600);
   timerForward(100, 300);
 
   //fourth ball
