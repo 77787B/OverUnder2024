658bb17fb397bf64dce6fd95c6c03cd97e0ae901
Amy3139 <144573648+Amy3139@users.noreply.github.com>
2024-02-13 19:55:02 -0800

    auton - near-1 (awp) - done

--- src/auton/near/near-1.cpp
+++ src/auton/near/near-1.cpp
@@ -6,13 +6,49 @@
 
 /**
  * Near 1: AWP: 1 goal, 2 over
- * 1. Get corner triball
- * 2. Push allowance triball into goal
+ * 1. Push allowance triball into goal
+ * 2. Get corner triball
  * 3. Push triballs over barrier
  * 4. Touch horizontal bar
 */
 
 void near_1() {
+
+  MyTimer autotimer;
+  autotimer.reset();
+  printf ("\n===== near_1: Start =====\n");
+
+  // Push alliance triball into goal
+  MyGps.gpsPIDMove(-500, 350 , 1, 50);
+  MyGps.gpsPIDMove(-500, 575 , 1, 25);
+  setIntakeSpeed(-100);
+  this_thread::sleep_for(500);
+  setIntakeSpeed(0);
+  MyGps.gpsPIDMove(-500, 450 , -1, 25);
+  PIDAngleRotateAbs(180);
+
+  // Get corner triball
+  setPistonFW(true);
+  PIDPosCurveAbs(300, 600, 25);
+  setPistonFW(false);
+  this_thread::sleep_for(500);
+  timerForward(-50, 200);
+  PIDAngleRotateAbs(145);
+  PIDPosCurveAbs(300, 500, 50);
+  PIDAngleRotateAbs(90);
+
+  // Push triballs over alley and touch horizontal bar
+  setIntakeSpeed(-100);
+  MyGps.gpsPIDMove(600, -150, 1);
+  this_thread::sleep_for(1000);
+
+  printf ("\n===== near_1: End: Elased=%.i =====\n", autotimer.getTime());
+
+  Brain.Screen.setCursor(11, 1);
+  Brain.Screen.print("AutonTimer: %d               ", autotimer.getTime());
+}
+
+void near_1_amy() {
 
   MyTimer autotimer;
   autotimer.reset();
