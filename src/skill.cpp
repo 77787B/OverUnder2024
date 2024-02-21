d9f4e0257edb849fa63471eae27c20acc9c6a1c0
Amy Li <amyziweili@gmail.com>
2024-02-20 18:57:20 -0800

    small update

--- src/skill.cpp
+++ src/skill.cpp
@@ -10,6 +10,8 @@ void skill(){
   MyTimer autotimer;
   autotimer.reset();
 
+  //Note: must use back wings, so change config for front and back to use curve
+
   //push in alliance triballs
   MyGps.gpsPIDMove(-315, -400, -1, 50);
   PIDAngleRotateAbs(0);
@@ -22,15 +24,33 @@ void skill(){
   setPistonBW(true);
   setCatapultSpeed(70);
   //this_thread::sleep_for(21000);
-  this_thread::sleep_for(1000);2
+  this_thread::sleep_for(1000);
   setCatapultSpeed(0);
   setPistonBW(false);
-  
 
   return;
   
+  //push middle balls to alley
   MyGps.gpsPIDMove(1100, -600, 1);
-  PIDAngleRotateAbs
+  setPistonBW(true);
+  MyGps.gpsPIDMove(1000, -2500, -1);
+  timerForward(70, 200);
+  timerForward(-100, 350);
+  timerForward(100, 200);
+
+  //push balls across alley and into goal
+  MyGps.gpsPIDMove(0, 0, 1);
+  //only set one side true
+  setPistonBW(true);
+  MyGps.gpsPIDMove(500, 75, -1);
+  MyGps.gpsPIDMove(1800, 75, -1);
+  MyGPs.gpsPIDMove(2200, 0, -1, 40);
+  PIDAngleRotateAbs(135);
+  // maybe set other wing true
+  // setPistonBW(true);
+  MyGPs.gpsPIDMove(2500, -200, -1, 50);
+  MyGPs.gpsPIDMove(2600, -600, -1, 50);
+
 
 
   Brain.Screen.setCursor(11, 1);
