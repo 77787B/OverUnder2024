ab879b2efcf539fc8ebcd50ebc7f582ac7d18248
amxndapxnda <amanda@feldmeier.us>
2024-04-23 23:14:58 -0700

    Final Version (Finished Most)

--- src/auton/far/far-3-amanda.cpp
+++ src/auton/far/far-3-amanda.cpp
@@ -5,21 +5,12 @@
 #include "GPS.h"
 #include "basic-functions.h"
 
-
-
-
 void far_3_amanda() {
-
-
-
 
 /* POSITIONING:
 CENTER (Any face) OF BLUE BALL --> BACK AGAINST BAR, BARELY TOUCHING ROBOT
 ORANGE BOX --> EDGE OF POLE (LEFT SIDE)
 BACK SLEDS --> EDGE AND RIGHT ON TOP OF BOX
-
-
-
 
 ISSUES:
 TRIBALL IS INTAKED TOO FAR --> IF THERE IS NO MORE RUBBERBAND.. ADD:
@@ -30,9 +21,6 @@ FRONT WINGS
 FIND WAY TO SMACK BALL WITH WING WHILE GETTING MIDDLE BALL
 DO THREADS TO DO MULTIPLE TASKS AT SAME TIME TO SAVE TIME
 
-
-
-
 STUFF:
 TIME NEEDED FOR ROBOT TO INTAKE BALL --> 200???
 TURN ON/OFF INTAKE AFTER REACHING POSITION NEEDED!
@@ -41,73 +29,76 @@ CHANGE THIS SO THE ROBOT IS CONSISTENT IN GRABBING TRIBALL!!!
 MyTimer autotimer;
 autotimer.reset();
 
+
 //BALL 1 - get ball in middle
 setIntakeSpeed(100);
-posForwardAbsWithHeading(90, 1400, 15);
+// posForwardAbsWithHeading(90, 1400, 15); - SLOWER CODE FOR GRABBING, UPDATED TO 100 SPEED
+posForwardAbsWithHeading(100, 1285, 15);
 PIDPosForwardAbs(1550);
 this_thread::sleep_for(100);
 // go back to red pole and spit out ball
 MyGps.gpsPIDMove(-200, 150, -1, 100);
-// posForwardAbsWithHeading(-100, 170, 18);
-// PIDPosForwardAbs(170);
 setIntakeSpeed(-100);
 PIDAngleRotateAbs(70);
 this_thread::sleep_for(100);
 
+
 //BALL 2 - get ball under hang pole
 setIntakeSpeed(100);
 PIDAngleRotateAbs(-50);
-PIDPosForwardAbs(870);
-this_thread::sleep_for(100);
+PIDPosForwardAbs(850);
+this_thread::sleep_for(150);
 //move backwards to bar after grabbing triball
 PIDAngleRotateAbs(-45);
 setIntakeSpeed(0);
-PIDPosForwardAbs(-950);//-920
+PIDPosForwardAbs(-930);//-920
+
 
 //BALL 3 - grab ball in corner with backwing
 PIDAngleRotateAbs(-75);
 setPistonBLW(true);
-PIDPosForwardRel(-420);//-400
-PIDAngleRotateAbs(-70);
+PIDPosForwardRel(-370);//-400
 setPistonBLW(false);
+PIDAngleRotateAbs(-75);
+
+
 
 
 //PUSH 3 BALLS INTO GOAL
-posForwardAbsWithHeading(20, -250, -130);
-setPistonBRW(true);//TEST
+posForwardAbsWithHeading(30, -250, -125);//?, ?, -130
+setPistonBRW(true);
 timerForward(-100, 350);
-timerForward(40, 100);
+timerForward(40, 200);//125
 this_thread::sleep_for(50);
-PIDAngleRotateAbs(55);
+// PIDAngleRotateAbs(-35);//TEST
+PIDAngleRotateAbs(60);
 setIntakeSpeed(-100);
+// setPistonFW(true);//TEST, CAN YOU MAKE IT SO THAT THE FRONT RIGHT WING IS OPEN
 timerForward(100, 400);
-this_thread::sleep_for(200);
+this_thread::sleep_for(100);//200
 setIntakeSpeed(0);
+setPistonFW(false);
 
-//UNNEEDED PART
-// PIDAngleRotateAbs(65);
-// setIntakeSpeed(-100);
-// this_thread::sleep_for(200);
-// timerForward(100, 400);
-// this_thread::sleep_for(150);
-// setIntakeSpeed(0);
 
 //BALL 4 - left triball at black pole
 PIDPosForwardRel(-200);
 setPistonBRW(false);
 PIDAngleRotateAbs(-45);
 setIntakeSpeed(100);
-posForwardAbsWithHeading(65, 1050, -360);
-PIDAngleRotateAbs(-360);
-this_thread::sleep_for(300);
+posForwardAbsWithHeading(75, 1100, -20);//-360
+PIDPosForwardAbs(1100);
+this_thread::sleep_for(100);
+
 
 setIntakeSpeed(0);
-PIDAngleRotateAbs(120);
-setIntakeSpeed(-100);
+PIDAngleRotateAbs(105);
 setPistonFW(true);
-posForwardAbsWithHeading(70, 900, 100);
+// posForwardAbsWithHeading(60, 900, 135);
+setIntakeSpeed(-100);
+PIDPosForwardAbs(1200);
+setIntakeSpeed(0);
+
 
 Brain.Screen.setCursor(11, 1);
 Brain.Screen.print("AutonTimer: %d               ", autotimer.getTime());
 }
-
