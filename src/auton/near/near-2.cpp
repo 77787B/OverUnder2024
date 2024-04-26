691b93c8dc125fa5c5dd2c2741db5b519bae0abb
Amy3139 <144573648+Amy3139@users.noreply.github.com>
2024-04-25 23:20:15 -0500

    near-2 AWP - use wing to push corner triball instead of rotate to prevent getting stuck (WIP, not tested in World field)

--- src/auton/near/near-2.cpp
+++ src/auton/near/near-2.cpp
@@ -13,140 +13,78 @@
 */
 
 void near_2(bool isRush) {
-  // PIDPosForwardAbs(120);
-  // PIDPosForwardAbs(120, 150);
-  // setIntakeSpeed(-100);
-  // this_thread::sleep_for(500);
-  // return;
-
   MyTimer autotimer;
   autotimer.reset();
   printf ("\n===== near_2: Start =====\n");
 
   // ## Rush to intake center ball
   setIntakeSpeed(100);
-  float forwardTarget = 1175;
+  float forwardTarget = 1215;
   // float forwardTarget = 1215;
   // float forwardTarget = 1230;
   if (isRush == false) {
     forwardTarget = 900;
-  }
+  } 
   PIDPosForwardAbs(forwardTarget);
   this_thread::sleep_for(300);
   setIntakeSpeed(0);
 
   // ## Drop off center ball
+  PIDAngleRotateAbs(-2);
   resetForwardPos();
-  PIDPosForwardAbs(50 - forwardTarget);
-  PIDAngleRotateAbs(115);
+  PIDPosForwardAbs(30 - forwardTarget);
+  PIDAngleRotateAbs(110);
   setIntakeSpeed(-100);
   this_thread::sleep_for(400);
 
   // ## Intake alliance triball
-  PIDAngleRotateAbs(220);
+  PIDAngleRotateAbs(233);
   setIntakeSpeed(100);
-  PIDPosForwardAbs(200);
+  PIDPosForwardAbs(175);
   this_thread::sleep_for(300);
   setIntakeSpeed(0);
 
   // ## Outtake alliance triball into goal
   PIDAngleRotateAbs(-60);
-  PIDPosForwardAbs(575);
-  PIDAngleRotateAbs(-18);
-  PIDPosForwardAbs(120, 200);
+  PIDPosForwardAbs(570, 100, 700);
+  PIDAngleRotateAbs(-15);
+  PIDPosForwardAbs(140, 100, 300);
   setIntakeSpeed(-100);
-  this_thread::sleep_for(1000);
-  setIntakeSpeed(0);
+  this_thread::sleep_for(500);
+  // setIntakeSpeed(0);
+  // this_thread::sleep_for(100);
 
   // ## Get corner triball
-  PIDPosForwardAbs(-75);
-  PIDAngleRotateAbs(-50);
-  // PIDPosForwardAbs(-325);
-  PIDPosForwardAbs(-300);
+  PIDPosForwardAbs(-45);
+  PIDAngleRotateAbs(-63);
   setPistonBLW(true);
   this_thread::sleep_for(100);
-
-  PIDAngleRotateAbs(-80);
+  PIDPosForwardAbs(-260, 100, 400);
+  // PIDAngleRotateAbs(-85);
   setPistonBLW(false);
-  this_thread::sleep_for(10);
+  this_thread::sleep_for(100);
 
   // ## Push triballs over alley and touch horizontal bar
-  PIDAngleRotateAbs(110);
+  // Back up a bit before rotate to prevent hitting the corner triball back into corner
+  resetForwardPos();
+  PIDPosForwardAbs(200);
+  PIDAngleRotateAbs(120);
   setIntakeSpeed(-100);
-  this_thread::sleep_for(100);
   setPistonFW(true);
-  PIDPosForwardAbs(380);
+  this_thread::sleep_for(100);
 
+  // PIDPosForwardAbs(400, 100, 700);
+  PIDPosCurveAbs(450, 750, 25); // use curve is safer then move forward.
+  // PIDPosCurveAbs(400, 700, 25);
+  // PIDPosCurveAbs(650, 850, 25);
   PIDAngleRotateAbs(76);
   setPistonFLW(false);
   this_thread::sleep_for(50);
-  PIDPosForwardAbs(775);
+  PIDPosForwardAbs(715);
   setPistonFW(false);
-  this_thread::sleep_for(500);
+  // this_thread::sleep_for(500);
 
   printf ("\n===== near_2: End: Elased=%.i =====\n", autotimer.getTime());
   Brain.Screen.setCursor(11, 1);
   Brain.Screen.print("AutonTimer: %d               ", autotimer.getTime());
-}
-
-void near_2_gps(bool isRush) {
-/*
-  MyTimer autotimer;
-  autotimer.reset();
-  printf ("\n===== near_2: Start =====\n");
-
-  // ## Rush to intake center ball
-  setIntakeSpeed(100);
-  if (isRush) {
-    MyGps.gpsPIDMove(0, 1200, 1);
-  } else {
-    MyGps.gpsPIDMove(0, 600, 1);
-  }
-  this_thread::sleep_for(300);
-  setIntakeSpeed(0);
-
-  // ## Drop off center ball
-  MyGps.gpsPIDMove(-15, 200, -1, 100);
-  PIDAngleRotateAbs(115);
-  setIntakeSpeed(-100);
-  this_thread::sleep_for(700);
-  setIntakeSpeed(0);
-
-  // ## Push alliance triball into goal
-  setIntakeSpeed(60);
-  MyGps.gpsPIDMove(-180, -45, 1, 20);
-  this_thread::sleep_for(100);
-  setIntakeSpeed(0);
-  MyGps.gpsPIDMove(-615, 200, 1, 50);
-  // MyGps.gpsPIDMove(-650, 305, 1, 20);
-  // PIDAngleRotateAbs(-15);
-  // timerForward(10, 325);
-  // Use this if it keeps getting stuck at comps:
-  PIDAngleRotateAbs(-15);
-  timerForward(17, 475);
-  this_thread::sleep_for(100);
-  setIntakeSpeed(-100);
-  this_thread::sleep_for(400);
-  setIntakeSpeed(0);
-  MyGps.gpsPIDMove(-645, 305, -1, 50);
-
-  // ## Get corner triball
-  setPistonBLW(true);
-  MyGps.gpsPIDMove(-360, 20, -1, 80);
-  PIDAngleRotateAbs(-110);
-  setIntakeSpeed(-100);
-  this_thread::sleep_for(100);
-  setPistonBLW(false);
-  setPistonFRW(true);
-
-  // ## Push triballs over alley and touch horizontal bar
-  MyGps.gpsPIDMove(-45, -100, 1, 40);
-  MyGps.gpsPIDMove(715, 65, 1, 80);
-  // this_thread::sleep_for(200);
-  // setPistonFRW(false);
-
-  printf ("\n===== near_2: End: Elased=%.i =====\n", autotimer.getTime());
-  Brain.Screen.setCursor(11, 1);
-  Brain.Screen.print("AutonTimer: %d               ", autotimer.getTime());
-*/
 }
